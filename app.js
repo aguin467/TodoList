33b877633f7b9fd1cfa2d8abe9059556861fd61e
aguin467 <a.guin003@gmail.com>
2020-01-15 18:52:54 -0500

    Adding files

---
+++ app.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const app = new Vue({
+	el: '#app',
+	data: {
+		title: 'The Coding A',
+		newTodo: '',
+		todos: []
+	},
+	methods: {
+		addTodo() {
+			this.todos.push({
+				title: this.newTodo,
+				done: false
+			});
+			this.newTodo = '';
+		},
+		removeTodo(todo) {
+			const todoIndex = this.todos.indexOf(todo);
+			this.todos.splice(todoIndex, 1);
+		},
+		allDone() {
+			this.todos.forEach(todo => {
+				todo.done = true;
+			});
+		},
+	}
+});
